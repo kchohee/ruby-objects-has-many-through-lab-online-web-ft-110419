b46b52296d7fc759fa182aa8846b2c559b871b88
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-21 20:55:20 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,7 +1,11 @@
 class Appointment
   attr_accessor :date, :patient, :doctor
   @@all = []
+<<<<<<< HEAD
   def initialize(date, patient, doctor)
+=======
+  def initialize(patient, date, doctor)
+>>>>>>> a8b890400698e4402da73d845f73a76a5dc7c2d5
     self.date = date
     self.patient = patient
     self.doctor = doctor
--- lib/patient.rb
+++ lib/patient.rb
@@ -6,15 +6,23 @@ class Patient
     @@all << self
   end
   def new_appointment(doc,day)
+<<<<<<< HEAD
     return Appointment.new(day,self, doc)
+=======
+    return Appointment.new(self,day, doc)
+>>>>>>> a8b890400698e4402da73d845f73a76a5dc7c2d5
   end
   def self.all
     return @@all
   end
   def appointments
+<<<<<<< HEAD
     return Appointment.all.filter {|a| a.patient == self}
   end
   def doctors
     self.appointments.collect{|a|a.doctor}
+=======
+    return Appointment.all.filter{|a| a.patient == self}
+>>>>>>> a8b890400698e4402da73d845f73a76a5dc7c2d5
   end
 end
