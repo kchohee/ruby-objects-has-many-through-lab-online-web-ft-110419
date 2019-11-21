ae8babeda91555ab9a00250e3de88659a0da6184
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-21 22:06:01 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -15,6 +15,6 @@ class Doctor
     return Appointment.new(my_date,my_person,self)
   end
   def patients
-    self.appointments.collect{|a| a.patient}
+    Appointments.collect{|a| a.patient}
   end
 end
